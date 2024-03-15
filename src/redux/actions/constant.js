f12b0f1383c918b0456b2bb74441bbfa570ff871
bahattinumur <bahattinumur@gmail.com>
2024-03-16 02:38:05 +0300

    second commit

--- src/redux/actions/constant.js
+++ src/redux/actions/constant.js
@@ -1,10 +1,9 @@
 export const options = {
-    headers: {
-      accept: 'application/json',
-      // API KEY
-      Authorization: 'Please Enter Here Your API KEY!'
-    }
-  };
-  
-//? Resimlerin başına eklenmesi gereken base URL
-  export const baseImgUrl = "https://image.tmdb.org/t/p/original"
+  headers: {
+    accept: "application/json",
+    // API KEY
+    Authorization: "Please Enter Here Your API KEY!",
+  },
+};
+
+export const baseImgUrl = "https://image.tmdb.org/t/p/original";
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -1,47 +1,47 @@
-
 import { options } from "./constant";
 import axios from "axios";
 import { ActionTypes } from "./reducers/actionTypes";
 
+// Bütün isteklerin main URL'i
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
-//? Bütün isteklerin Base URL'i
-axios.defaults.baseURL="https://api.themoviedb.org/3";
-
-
-
-//? Popüler filmleri getir ve store'a aktar
+// Popüler filmleri getir ve store'a aktar.
 export const getPopular = () => (dispatch) => {
-
-//? Reducer'a yüklemenin başladığına haber ver
-dispatch({
-    type:ActionTypes.SET_MOVIES_LOADING,
-})
-    axios
-    .get('/movie/popular?language=en', options)
-//? Olumlu olursa reducer a veriyi aktar
-    .then((res) => dispatch ({
-        type:ActionTypes.SET_MOVIES,
-        payload:res.data.results}))
-//? Olumsuz olursa reducer a hatayı aktar       
-        .catch((err) => {
-            dispatch({
-                type:ActionTypes.SET_MOVIES_ERROR,
-                payload:err.message
-            })
-        })
+  // Reducer'a yüklemenin başladığını haber ver.
+  dispatch({
+    type: ActionTypes.SET_MOVIES_LOADING,
+  });
+  axios
+    .get("/movie/popular?language=en", options)
+    // Eğer olumlu olursa reducer'a veriyi aktar.
+    .then((res) =>
+      dispatch({
+        type: ActionTypes.SET_MOVIES,
+        payload: res.data.results,
+      })
+    )
+    // Olumsuz olursa reducer'a hatayı ilet.
+    .catch((err) => {
+      dispatch({
+        type: ActionTypes.SET_MOVIES_ERROR,
+        payload: err.message,
+      });
+    });
 };
 
-
-//? kategorileri al ve store'a aktar 
+// Kategorileri al ve store'a aktar.
 
 export const getGenres = () => (dispatch) => {
-    dispatch({type:ActionTypes.SET_GENRES_LOADING})
-    axios.get('genre/movie/list?language=en', options)
-    .then((res)=>{
-        dispatch({type:ActionTypes.SET_GENRES,payload:res.data.genres});
+  dispatch({ type: ActionTypes.SET_GENRES_LOADING });
+  axios
+    .get("genre/movie/list?language=en", options)
+    .then((res) => {
+      dispatch({ type: ActionTypes.SET_GENRES, payload: res.data.genres });
     })
-    .catch(err => dispatch({
-        type:ActionTypes.SET_GENRES_ERROR,
-        payload:err.message,
-    }))
-}
+    .catch((err) =>
+      dispatch({
+        type: ActionTypes.SET_GENRES_ERROR,
+        payload: err.message,
+      })
+    );
+};
